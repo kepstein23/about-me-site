b720faa96cf1db12bd6e4a92002bf2e2b29a9643
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-08-12 13:35:20 -0400

    break out components into textbox

--- src/App.js
+++ src/App.js
@@ -3,12 +3,67 @@ import './App.css';
 import React from 'react';
 import {Button} from './components/Button';
 import { TextBox } from './components/TextBox';
+import headshot from "./headshot.jpg"
 
 function App() {
 
   function handleRedirect(link) {
     window.open(link, '_blank')
   }
+
+  const aboutMeText = [
+    {
+      name: "img",
+      content: headshot,
+      width: "250",
+      height:"200"
+
+    },
+    {
+      name: "text", 
+      content: "Hi, my name is Kimberly Epstein.  I am a Junior at University of Pennsylvania pursuing a double major in Computer Science and Cognitive Science with minors in Consumer Psychology and Linguistics. I am passionate about the intersection of mind and computer science, and have special interests in Web Development, Artificial Intelligence and Natural Language Processing."
+    }
+    
+]
+
+  const experiencesTexts = [
+    {
+      name:"subtitle",
+      content:"Computational Social Science Lab"
+    },
+    {
+      name:"text",
+      content:"As a research asisstant, I worked to develop and implement a project focused on reducing affective polarization in group disucssions. Using React, JavaScript, and HTML, I developed an easy-to-navigate interface for participants to engage in live group discussions.  This experience provided an opportunity for me to apply computer science priciples to the study of mind and brain, developing my empirical awareness as well as technical skills."
+    },
+    {
+      name:"text",
+      href:"https://docs.google.com/presentation/d/1iRkXuWYpIyld8FRK6e6IlNE45Fd3fXjtnF8kvm-1K1Q/edit#slide=id.g13ed3caaa68_0_6",
+      content:"Click here to learn more about my contributions to the project."
+    },
+    {
+      name:"subtitle",
+      content:"Institute for Human Machine Cognition"
+    },
+    {
+      name:"text",
+      content:"As a research intern, I designed, coded, and conducted a survey to ollect data related to how participants perceive credibility of tweets related to COVID-19.  I first wrote a python script to extract relevant tweets from over 400 Twitter users, then built the survey using HTML, CSS, and PHP.  This experience allowed me to combine my interests of computer science and cognitive science, using cognitive science pricinples to design and conduct the study, but computer science techniques to actually build the survey."
+    }
+  ]
+
+  const relevantCoursework = [
+    {
+      name:"subtitle",
+      href:"https://catalog.upenn.edu/search/?P=CIS%201210",
+      title:"title",
+      content:"CIS 1210"
+    },
+    {
+      name:"text",
+      content:"Completed Programming Sequence Intro-Programming II"
+    },
+
+  ]
+
 
 
   return (
@@ -28,11 +83,41 @@ function App() {
         </div>
       </div>
       <div style={{padding:"5px"}}>
-        <TextBox 
+        <div className="textBox">
+          <h1>About Me</h1>
+          <div style={{padding: "5px", display:"flex"}}>
+            <div style={{padding: "5px"}}>
+            < img src={headshot} height="184" width="225"></img>
+            </div>
+            <div style={{padding: "5px"}}>
+              <p>
+                Hi, my name is Kimberly Epstein.  I am a Junior at University of Pennsylvania pursuing a double major 
+                in Computer Science and Cognitive Science with minors in Consumer Psychology and Linguistics. I am passionate
+                about the intersection of mind and computer science, and have special interests in Artificial Intelligence and 
+                Natural Language Processing.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <TextBox 
           title="About me"
-          subtitle="learn about me"
-          text="this is me"
+          texts={aboutMeText}
+          // text="Hi, my name is Kimberly Epstein.  I am a Junior at University of Pennsylvania pursuing a double major 
+          //   in Computer Science and Cognitive Science with minors in Consumer Psychology and Linguistics. I am passionate
+          //   about the intersection of mind and computer science, and have special interests in Artificial Intelligence and 
+          //   Natural Language Processing."
+        /> */}
+        <p></p>
+        <TextBox
+          title="Recent Experiences"
+          texts={experiencesTexts}
         />
+        <p></p>
+        <TextBox
+          title="Relevant Coursework"
+          texts={relevantCoursework}
+        />
+        <p></p>
       </div>
       
     </div>
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,11 +1,39 @@
 import React from "react"
 
-export function TextBox({title, subtitle, text}) {
+export function TextBox({title, texts}) {
+    function displayText()  {
+        let toDisplay = []
+        let jsx = []
+
+        texts.forEach((item) => {
+            if (item.name === "subtitle") {
+                if (item.href) {
+                    toDisplay.push(<h3><a href={item.href} target="_blank">{item.content}</a></h3>)
+                } else {
+                    toDisplay.push(<h3>{item.content}</h3>)
+                }
+                
+            } else if (item.name === "text") {
+                if (item.href) {
+                    toDisplay.push(<p><a href={item.href} target="_blank">{item.content}</a></p>)
+                } else {
+                    toDisplay.push(<p>{item.content}</p>)
+                }
+                
+            } else if (item.name === "img") {
+                toDisplay.push(<img src={item.content} height={item.height} width={item.width}/>)
+            }
+        })
+        return toDisplay
+        
+    }
+
     return (
         <div className="textBox">
             <h1 class="title">{title}</h1>
-            <h2>{subtitle}</h2>
-            <p>{text}</p>
+            {displayText()}
+            {/* <h2>{subtitle}</h2>
+            <p>{text}</p> */}
         </div>
     )
 }
